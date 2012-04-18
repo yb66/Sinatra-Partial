a1c73c3550bfdbc7b3aff59fb201d2962f0d72a5
Iain Barnett <iainspeed@gmail.com>
2012-04-18 23:42:36 +0100

    Pending specs for the innards.

---
+++ spec/sinatra_partial_private_spec.rb
@@ -0,0 +1,13 @@
+# encoding: UTF-8
+
+require_relative "./spec_helper.rb"
+require_relative "../lib/sinatra/partial.rb"
+
+
+describe "partial_expand_path" do
+  pending
+end
+
+describe "Helpers::partial" do
+  pending
+end
