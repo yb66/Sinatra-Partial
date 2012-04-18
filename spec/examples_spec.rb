1d20c8f40da9a0b97e7dd84f0a01c7152c9827ba
Iain Barnett <iainspeed@gmail.com>
2012-04-18 23:02:10 +0100

    Spec that runs against all the examples.

---
+++ spec/examples_spec.rb
@@ -0,0 +1,31 @@
+# encoding: UTF-8
+
+require_relative "./spec_helper.rb"
+
+require_relative "../lib/sinatra/partial.rb"
+
+require_relative "../examples/app_no_underscores/app.rb"
+require_relative "../examples/app_with_underscores/app.rb"
+require_relative "../examples/app_with_underscores_and_erb/app.rb"
+require_relative "../examples/app_with_underscores_and_erb_and_subdirs/app.rb"
+
+shared_examples_for "all in examples dir" do
+  let(:expected) { "<html><head></head><body><p>Time is #{Time.now}</p><ul><li>This</li><li>is</li><li>all</li><li>new</li></ul><p>A is A</p><p>B is B</p><p>C is C</p><p>D is D</p><p>Hello, World</p></body></html>" }
+  subject { browser.last_response }
+  it { should be_ok }
+  it { subject.body.should == expected }
+end
+
+def new_session( app )
+  Rack::Test::Session.new(Rack::MockSession.new( app ) )
+end
+
+Apps = [AppNoUnderscores, AppWithUnderscores, AppWithUnderscoresAndErb, AppWithUnderscoresAndErbAndSubdirs]
+
+Apps.each do |app|
+  describe app.to_s do
+    let(:browser){  new_session( app ) }
+    before{ browser.get '/' }
+    it_should_behave_like "all in examples dir"
+  end
+end
