130da7ca2e4e29b275335988611d369a3e9f758f
Iain Barnett <iainspeed@gmail.com>
2012-04-18 18:34:25 +0100

    Added spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+# encoding: UTF-8
+
+require 'rspec'
+Spec_dir = File.expand_path( File.dirname __FILE__ )
+
+# code coverage
+require 'simplecov'
+SimpleCov.start
+
+require "logger"
+logger = Logger.new STDOUT
+logger.level = Logger::DEBUG
+logger.datetime_format = '%a %d-%m-%Y %H%M '
+LOgger = logger
+
+
+Dir[ File.join( Spec_dir, "/support/**/*.rb")].each do |f| 
+  puts "requiring #{f}"
+  require f
+end
