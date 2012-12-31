8bf0d7385bfd56417de7b8309466f964bd6d15c3
Iain Barnett <iainspeed@gmail.com>
2012-12-31 12:23:21 +0000

    Added specs and examples for slim, almost passes, need to work out how slim handles dynamic hashes as html attributes so I can turn of automatic escaping of them.

--- spec/examples_spec.rb
+++ spec/examples_spec.rb
@@ -8,6 +8,7 @@ require_relative "../examples/app_no_underscores/config.rb"
 require_relative "../examples/app_with_underscores/config.rb"
 require_relative "../examples/app_with_underscores_and_erb/config.rb"
 require_relative "../examples/app_with_underscores_and_erb_and_subdirs/config.rb"
+require_relative "../examples/app_no_underscores_and_slim/config.rb"
 
 shared_examples_for "all in examples dir" do
   let(:expected) { "<html><head></head><body><p>Time is #{Time.now}</p><ul><li class='klassic'>This</li><li class='klassic'>is</li><li class='klassic'>all</li><li class='klassic'>new</li></ul><p>A is A</p><p>B is B</p><p>C is C</p><p>D is D</p><p>Hello, World</p><p>Show me magic!</p></body></html>" }
@@ -20,7 +21,7 @@ def new_session( app )
   Rack::Test::Session.new(Rack::MockSession.new( app ) )
 end
 
-Apps = [AppNoUnderscores, AppWithUnderscores, AppWithUnderscoresAndErb, AppWithUnderscoresAndErbAndSubdirs]
+Apps = [AppNoUnderscores, AppWithUnderscores, AppWithUnderscoresAndErb, AppWithUnderscoresAndErbAndSubdirs,AppNoUnderscoresAndSlim]
 
 Apps.each do |app|
   describe app.to_s do
