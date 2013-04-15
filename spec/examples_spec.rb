bf316a0f515e7fb1b83dd0172f17305366d744ea
Iain Barnett <iainspeed@gmail.com>
2013-04-15 19:19:05 +0100

    Separated out specs that use block argument.

--- spec/examples_spec.rb
+++ spec/examples_spec.rb
@@ -8,7 +8,7 @@ require_relative "../examples/app_no_underscores/config.rb"
 require_relative "../examples/app_with_underscores/config.rb"
 require_relative "../examples/app_with_underscores_and_erb/config.rb"
 require_relative "../examples/app_with_underscores_and_erb_and_subdirs/config.rb"
-require_relative "../examples/app_no_underscores_and_slim/config.rb"
+#require_relative "../examples/app_no_underscores_and_slim/config.rb"
 
 
 shared_context "Running different apps" do |c|
@@ -25,12 +25,21 @@ shared_examples_for "all in examples dir" do
 end
 
 
-[AppNoUnderscores, AppWithUnderscores, AppWithUnderscoresAndErb, AppWithUnderscoresAndErbAndSubdirs
+[AppWithUnderscores, AppWithUnderscoresAndErb, AppWithUnderscoresAndErbAndSubdirs
 ].each do |c|
   describe c.to_s do
     include_context "Running different apps", c
     it_should_behave_like "all in examples dir" do
       let(:expected) { "<html><head></head><body><p>Time is #{Time.now}</p><ul><li class='klassic'>This</li><li class='klassic'>is</li><li class='klassic'>all</li><li class='klassic'>new</li></ul><p>A is A</p><p>B is B</p><p>C is C</p><p>D is D</p><p>Hello, World</p><p>Show me magic!</p></body></html>" }
+    end
+  end
+end
+
+[AppNoUnderscores,AppNoUnderscoresAndSlim].each do |c|
+  describe c.to_s do
+    include_context "Running different apps", c
+    it_should_behave_like "all in examples dir" do
+      let(:expected) { %q!<html><head></head><body><p>Time is #{Time.now}</p><ul><li class='klassic'>This</li><li class='klassic'>is</li><li class='klassic'>all</li><li class='klassic'>new</li></ul><p>A is A</p><p>B is B</p><p>C is C</p><p>D is D</p><p>Hello, World</p><p>Show me magic</p></body></html><div class="container"><a href="/mylink">This is is a link</a></div>! }
     end
   end
 end
