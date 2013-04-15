1388680691668c4951b1890c53e958edf2532bb0
Iain Barnett <iainspeed@gmail.com>
2013-04-15 19:15:11 +0100

    Generalised and cleaned up the spec so it can be used against two different tests easier, as it'll be used against the block changes in the slim branch.

--- spec/examples_spec.rb
+++ spec/examples_spec.rb
@@ -10,25 +10,27 @@ require_relative "../examples/app_with_underscores_and_erb/config.rb"
 require_relative "../examples/app_with_underscores_and_erb_and_subdirs/config.rb"
 #require_relative "../examples/app_no_underscores_and_slim/config.rb"
 
+
+shared_context "Running different apps" do |c|
+  include Rack::Test::Methods
+  let(:app){ c.app }
+  before{ get '/' }
+end
+
+
 shared_examples_for "all in examples dir" do
-  let(:expected) { "<html><head></head><body><p>Time is #{Time.now}</p><ul><li class='klassic'>This</li><li class='klassic'>is</li><li class='klassic'>all</li><li class='klassic'>new</li></ul><p>A is A</p><p>B is B</p><p>C is C</p><p>D is D</p><p>Hello, World</p><p>Show me magic!</p></body></html>" }
-  subject { browser.last_response }
+  subject { last_response }
   it { should be_ok }
-  it { subject.body.should == expected }
+  its(:body) { should == expected }
 end
 
-def new_session( app )
-  Rack::Test::Session.new(Rack::MockSession.new( app ) )
-end
 
-Apps = [AppNoUnderscores, AppWithUnderscores, AppWithUnderscoresAndErb, AppWithUnderscoresAndErbAndSubdirs,
-#AppNoUnderscoresAndSlim
-]
-
-Apps.each do |app|
-  describe app.to_s do
-    let(:browser){  new_session( app.app ) }
-    before{ browser.get '/' }
-    it_should_behave_like "all in examples dir"
+[AppNoUnderscores, AppWithUnderscores, AppWithUnderscoresAndErb, AppWithUnderscoresAndErbAndSubdirs
+].each do |c|
+  describe c.to_s do
+    include_context "Running different apps", c
+    it_should_behave_like "all in examples dir" do
+      let(:expected) { "<html><head></head><body><p>Time is #{Time.now}</p><ul><li class='klassic'>This</li><li class='klassic'>is</li><li class='klassic'>all</li><li class='klassic'>new</li></ul><p>A is A</p><p>B is B</p><p>C is C</p><p>D is D</p><p>Hello, World</p><p>Show me magic!</p></body></html>" }
+    end
   end
 end
